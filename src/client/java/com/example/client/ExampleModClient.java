21841ecc3a405fed3d22dc84cc7414a0e12a7a6c
sfhfbbbdvgfvbbhm <shadowexe1949@gmail.com>
2026-05-18 15:31:07 +0400

    Update ExampleModClient.java

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,10 +1,51 @@
-package com.example.client;
+package net.fabricmc.example;
 
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
-public class ExampleModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+public class ExampleMod implements ModInitializer {
+
+    private static KeyBinding spawnBotKey;
+    private static int botIdCounter = -2000;
+
+    @Override
+    public void onInitialize() {
+        spawnBotKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.cheat.spawnbot",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_Y,
+                "category.cheat.main"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (spawnBotKey.wasPressed()) {
+                if (client.world != null && client.player != null) {
+                    
+                    OtherClientPlayerEntity fakePlayerEntity = new OtherClientPlayerEntity(
+                            client.world, 
+                            client.player.getGameProfile()
+                    );
+
+                    fakePlayerEntity.refreshPositionAndAngles(
+                            client.player.getX(), 
+                            client.player.getY(), 
+                            client.player.getZ(), 
+                            client.player.getYaw(), 
+                            client.player.getPitch()
+                    );
+
+                    botIdCounter--;
+                    client.world.addEntity(botIdCounter, fakePlayerEntity);
+
+                    client.player.sendMessage(Text.literal("[SYSTEM] Fake player successfully deployed."), false);
+                }
+            }
+        });
+    }
 }
